878edca3a8bf50937a564f4385c995bf1775a266
Sina-Sche <71880295+Sina-Sche@users.noreply.github.com>
2020-11-24 16:14:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cra-with-api
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
