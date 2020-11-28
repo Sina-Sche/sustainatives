546dc9a239c04b2bafeb38bebcc980e04680b625
Sina-Sche <si.scheckenbach@gmail.com>
2020-11-28 14:46:21 +0100

    Change Card to LargeCard for better naming

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import { Header } from "../components/Header";
-import { Card } from "../components/Card";
+import { Card } from "../components/LargeCard";
 import { ProductList } from "../components/ProductList";
 import { NavBar } from "../components/NavBar";
 
