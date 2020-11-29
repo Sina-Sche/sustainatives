40454c5dfdd8c6991d55c5180b1f7398d4b55a34
Sina-Sche <si.scheckenbach@gmail.com>
2020-11-29 18:22:07 +0100

    Add useLocation to NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import GlobalStyle from "./GlobalStyle";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 
 function App() {
