26a19758e9a65fe57e08ac013780fb4437830e10
Sina-Sche <si.scheckenbach@gmail.com>
2020-12-03 20:44:48 +0100

    Set Limit to 8 Products in API Request

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,11 @@
 export async function getProducts() {
-  const result = await fetch("http://localhost:7001/products/");
+  const result = await fetch("http://localhost:7001/products/?_limit=8");
+  const products = result.json();
+  return products;
+}
+
+export async function getProductsByTitle(title) {
+  const result = await fetch(`http://localhost:7001/products/?title=${title}`);
   const products = result.json();
   return products;
 }
