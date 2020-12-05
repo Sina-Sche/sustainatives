7dbd2c49ef464833272c4c2328fbe226aa9d62dc
Sina-Sche <si.scheckenbach@gmail.com>
2020-12-05 12:07:36 +0100

    Test deployment without stories

--- client/src/stories/CategoryCard.stories.js
+++
@@ -1,9 +0,0 @@
-import CategoryOverview from "../components/CategoryOverview";
-export default {
-  title: "Components/Cards",
-  component: "CategoryList",
-};
-
-const Template = (args) => <CategoryOverview {...args} />;
-
-export const CategoryListA = Template.bind({});
--- client/src/stories/NavBar.stories.js
+++
@@ -1,8 +0,0 @@
-import NavBar from "../components/NavBar";
-
-export default {
-  title: "Components/NavBar",
-  component: NavBar,
-};
-
-export const NavBarInactive = () => <NavBar />;
