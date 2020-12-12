7463dbf8d0fcad5f0469d8e5e064eb11cd047d2c
Sina-Sche <si.scheckenbach@gmail.com>
2020-12-12 12:24:35 +0100

    Add .env, and connection to mongo via server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const jsonServer = require("json-server");
-const router = jsonServer.router("db.json");
-const middleware = jsonServer.defaults();
 
-const port = process.env.PORT || 3600;
+const { connect } = require("./lib/database");
 const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3600;
 
 app.use(express.static("public"));
 
@@ -15,12 +15,18 @@ app.use(
   express.static(path.join(__dirname, "client/storybook-static"))
 );
 
-app.use(middleware);
-app.use("/api", router);
 app.get("*", (request, response) => {
   response.sendFile(path.join(__dirname, "client/public", "index.html"));
 });
 
+async function run() {
+  console.log("On your way to the database...");
+  await connect(process.env.DB_URI, process.env.DB_NAME);
+  console.log("Success! You are now connected to the database");
+}
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+run();
