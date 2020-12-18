c7b9946014239acae508257e3a238ce08bc22927
Sina-Sche <si.scheckenbach@gmail.com>
2020-12-18 16:59:56 +0100

    Implement useQuery on FavoritePage

--- client/src/pages/FavoritePage.js
+++ client/src/pages/FavoritePage.js
@@ -2,29 +2,29 @@ import InfoBox from "../components/InfoBox";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import useFavorites from "../hooks/useFavorites";
-import { getProductById } from "../utils/api";
-import { useEffect, useState } from "react";
+import { getFavorites } from "../utils/api";
+import { useEffect } from "react";
+import { useQuery } from "react-query";
 
 export const FavoritePage = () => {
   const { toggleFavorite, favorites } = useFavorites("favorites", []);
-  const [favoriteData, setFavoriteData] = useState();
+  const { data, error, isError, isLoading, refetch } = useQuery(
+    ["products", favorites],
+    () => getFavorites(favorites)
+  );
 
   useEffect(() => {
-    const getData = async () => {
-      const promiseData = favorites.map(async (id) => getProductById(id));
-      const newData = await Promise.all(promiseData);
-      setFavoriteData(newData);
-      return newData;
-    };
-    getData();
-  }, [favorites]);
+    refetch(favorites);
+  }, [favorites, refetch]);
 
   return (
     <>
       <Header title={"Favorites"} />
       <h2>My favorite Products</h2>
-      {favoriteData &&
-        favoriteData.map((product) => {
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>{error.message}</div>}
+      {data &&
+        data.map((product) => {
           return (
             <InfoBox
               key={product.id}
