bc4071837887d4e081edec9f5639086862b158a2
Sina-Sche <si.scheckenbach@gmail.com>
2020-12-21 18:46:37 +0100

    Add filter function to SearchPage

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -10,12 +10,36 @@ import useFavorites from "../hooks/useFavorites";
 import useDebounce from "../hooks/useDebounce";
 import PageWrapper from "../components/PageWrapper";
 import useActive from "../hooks/useActive";
+import styled from "styled-components";
+import { ReactComponent as Filter } from "../assets/icons/filter.svg";
+
+const Button = styled.button`
+  border: 1px solid var(--secondary-color);
+  border-radius: 50px;
+  padding: 10px;
+  background: var(--secondary-color);
+  width: 90%;
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  line-height: 1;
+  h3 {
+    font-size: 1rem;
+    color: var(--primary-color);
+  }
+  svg {
+    height: 20px;
+    width: auto;
+    fill: var(--primary-color);
+  }
+`;
 
 export const SearchPage = () => {
   const { favorites, toggleFavorite } = useFavorites("favorites", []);
-  const [searchCategory, setSearchCategory] = useState("");
+  const [filterData, setFilterData] = useState("");
   const { activeCategories, toggleActive } = useActive([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [visible, setVisible] = useState(false);
   const debouncedSearchTerm = useDebounce(searchTerm, 400);
   const { data, isLoading, isError, error, refetch } = useQuery(
     ["products", debouncedSearchTerm],
@@ -24,25 +48,45 @@ export const SearchPage = () => {
   );
 
   useEffect(() => {
-    searchByCategory();
-  }, [activeCategories]);
-  const searchByCategory = async () => {
-    const data = await getProductsByCategory(activeCategories);
-    console.log(data);
-    return data;
-  };
-  useEffect(() => {
+    if (debouncedSearchTerm && activeCategories) {
+      filterByCategory();
+    }
     if (debouncedSearchTerm) {
       refetch();
     }
-  }, [debouncedSearchTerm, refetch]);
+  }, [debouncedSearchTerm, refetch, activeCategories]);
+
+  const filterByCategory = async () => {
+    if (activeCategories && data) {
+      const product = data
+        ?.filter((product) => {
+          return product.categories.includes(activeCategories.toString());
+        })
+        .map((product) => {
+          return product;
+        });
+      setFilterData(product);
+      return product;
+    } else {
+      const filterAll = async () => {
+        const filterAllData = await getProductsByCategory(activeCategories);
+        setFilterData(filterAllData);
+        return filterData;
+      };
+      filterAll();
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    searchByCategory();
+    filterByCategory();
   };
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
+  };
+
+  const handleShow = () => {
+    setVisible(!visible);
   };
 
   return (
@@ -52,27 +96,47 @@ export const SearchPage = () => {
         type={"search"}
         value={searchTerm}
         onChange={handleChange}
-        onSubmit={(e) => e.preventDefault()}
+        onSubmit={handleSubmit}
         placeholder={"What are you looking for?"}
       />
-      <CategoryList
-        activeCategories={activeCategories}
-        toggleActive={toggleActive}
-      />
+      <Button onClick={handleShow}>
+        <Filter />
+        <h3>Filter by categories</h3>
+      </Button>
+      {visible && (
+        <CategoryList
+          visible={visible}
+          activeCategories={activeCategories}
+          toggleActive={toggleActive}
+        />
+      )}
+
       {searchTerm && <h2>Your search results for {searchTerm}</h2>}
       {isLoading && <div>Loading...</div>}
       {isError && <div>{error.message}</div>}
-      {data?.map((product) => {
-        return (
-          <InfoBox
-            key={product._id}
-            size={"small"}
-            {...product}
-            onClick={() => toggleFavorite(product.id)}
-            isFavorite={favorites.includes(product.id)}
-          />
-        );
-      })}
+      {filterData?.length > 0
+        ? filterData.map((product) => {
+            return (
+              <InfoBox
+                key={product._id}
+                size={"small"}
+                {...product}
+                onClick={() => toggleFavorite(product.id)}
+                isFavorite={favorites.includes(product.id)}
+              />
+            );
+          })
+        : data?.map((product) => {
+            return (
+              <InfoBox
+                key={product._id}
+                size={"small"}
+                {...product}
+                onClick={() => toggleFavorite(product.id)}
+                isFavorite={favorites.includes(product.id)}
+              />
+            );
+          })}
       <NavBar />
     </PageWrapper>
   );
