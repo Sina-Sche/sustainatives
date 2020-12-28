b0a4f975caf3b99a8b03b6533be1daf4aaa7a0bd
Sina-Sche <si.scheckenbach@gmail.com>
2020-12-28 09:47:48 +0100

    Small improvements of DetailsPage

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -1,9 +1,10 @@
 import styled from "styled-components/macro";
-import Image from "../components/Image";
 import PropTypes from "prop-types";
+import Image from "../components/Image";
 import ProductText from "./ProductText";
 import icons from "./CategoryIcons";
 import useFavorites from "../hooks/useFavorites";
+import { ReactComponent as Visit } from "../assets/icons/go.svg";
 
 const DetailsContainer = styled.div`
   width: 100%;
@@ -12,34 +13,33 @@ const DetailsContainer = styled.div`
   flex-direction: column;
   padding-top: 10px;
   align-items: center;
+  color: var(--primary-color);
   div {
-    margin-top: 0;
-    padding-top: 0;
+    margin-top: 10px;
     min-width: 350px;
   }
-  h3 {
+  h3,
+  h5,
+  h6 {
+    color: var(--primary-color);
     margin-top: 0;
     padding-top: 0;
-    text-align: left;
-    color: var(--primary-color);
-  }
-  h5 {
-    color: var(--primary-color);
     text-align: left;
   }
   p {
     color: var(--primary-color);
-    height: auto;
-    display: flex;
-    overflow: visible;
+    -webkit-line-clamp: 8;
+    margin: 0 5px 0;
   }
   img {
-    border: 1px solid var(--icon-active-color);
+    border: 1px solid var(--secondary-color);
   }
-
   a {
     align-self: flex-end;
     width: 40%;
+  }
+  li {
+    text-align: center;
   }
 `;
 
@@ -57,29 +57,46 @@ const Button = styled.button`
   border-radius: 50px;
   color: var(--secondary-color);
   font-weight: bold;
-  padding: 8px;
   margin: 15px;
+  padding: 8px;
   letter-spacing: 1;
   font-size: 0.9rem;
+  display: flex;
+  align-items: center;
+  width: 80%;
+  svg {
+    fill: var(--secondary-color);
+    height: 20px;
+    justify-self: flex-start;
+    margin: 0 10px 0;
+  }
 `;
 
 const IconContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-  text-align: center;
   margin: 10px;
   svg {
     fill: var(--primary-color);
+  }
+  p {
+    font-size: 0.8rem;
+    text-align: center;
   }
 `;
 
 const ProductDetails = (data) => {
   const { favorites, toggleFavorite } = useFavorites("favorites", []);
   const productCategories = data.categories.map((category) => icons[category]);
-  const productCategoryIcons = Object.entries(
-    productCategories
-  ).map(([categoryName, { icon: Icon }]) => <Icon key={categoryName} />);
+  const productCategoryIcons = Object.entries(productCategories).map(
+    ([categoryName, { icon: Icon, label }]) => (
+      <li key={categoryName}>
+        <Icon />
+        <p>{label}</p>
+      </li>
+    )
+  );
   return (
     <DetailsContainer>
       <Image
@@ -93,7 +110,10 @@ const ProductDetails = (data) => {
         <ProductText {...data} />
         <IconContainer>{productCategoryIcons}</IconContainer>
         <a href={data.url}>
-          <Button>Visit Website</Button>
+          <Button>
+            <Visit />
+            Website
+          </Button>
         </a>
       </ProductDetailsContainer>
     </DetailsContainer>
