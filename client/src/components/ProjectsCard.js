f2c4d7655cd78a33ee4a47d02f74e17a89f26e11
Sina-Sche <si.scheckenbach@gmail.com>
2021-01-08 19:32:40 +0100

    Add ProjectPage with ProjectCard

---
+++ client/src/components/ProjectsCard.js
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components/macro";
+import icons from "./CategoryIcons";
+import PropTypes from "prop-types";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px auto;
+  width: 90%;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid var(--secondary-color);
+  border-radius: 50px;
+  img {
+    width: 280px;
+    border-radius: 50px;
+    padding-top: 20px;
+    margin: auto;
+  }
+  h3,
+  h4 {
+    margin: 0;
+    padding: 0;
+  }
+  div {
+    display: flex;
+    justify-content: center;
+  }
+  svg {
+    fill: var(--secondary-color);
+    height: 40px;
+    margin: 10px;
+  }
+`;
+
+const ProjectsCard = (project) => {
+  const projectCategories = project.categories.map(
+    (category) => icons[category]
+  );
+  const projectCategoryIcons = Object.entries(
+    projectCategories
+  ).map(([categoryName, { icon: Icon }]) => <Icon key={categoryName} />);
+  return (
+    <Container>
+      <img src={project.image} alt={project.project_name} />
+      <h3>{project.project_name}</h3>
+      <h4>{project.continent}</h4>
+      <div>{projectCategoryIcons}</div>
+    </Container>
+  );
+};
+
+export default ProjectsCard;
+
+ProjectsCard.propTypes = {
+  project: PropTypes.object,
+};
---
+++ client/src/pages/ProjectsPage.js
@@ -0,0 +1,22 @@
+import { useQuery } from "react-query";
+import { getAllProjects } from "../utils/api";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+import PageWrapper from "../components/PageWrapper";
+import ProjectsCard from "../components/ProjectsCard";
+
+const ProjectsPage = () => {
+  const { data } = useQuery(["projects"], async () => await getAllProjects());
+  console.log(data);
+  return (
+    <PageWrapper>
+      <Header title={"Projects"} />
+      {data?.map((project) => {
+        return <ProjectsCard key={project._id} {...project} />;
+      })}
+      <NavBar />
+    </PageWrapper>
+  );
+};
+
+export default ProjectsPage;
